8fa3aed4f5d54c3950e6fea5dc5667dc6d1dc5dc
Ed Landamore <mail@ratship.net>
2020-11-25 15:25:25 +0000

    data getter working

--- fivestar/data.py
+++ fivestar/data.py
@@ -5,27 +5,38 @@ Functions for handling data stuff
 import numpy as np
 import pandas as pd
 from fivestar.params import BUCKET_NAME, BUCKET_TRAIN_DATA_PATH, PROJECT_NAME
+from fivestar.params import LISTINGS_COLUMNS
 from google.cloud import storage
+import gcsfs
 
-def get_data(nrows=10000, local=False, optimize=False, **kwargs):
+
+def get_data(file='listings', nrows=None, local=True, optimize=False, **kwargs):
     """method to get the training data (or a portion of it) from google cloud bucket"""
-    # Add Client() here
-    filename = 'listings.csv'
-    client = storage.Client.from_service_account_json(
-        '/Users/ed/code/fivestar/star-project-key.json',
-        project=PROJECT_NAME)
-    if local:
-        path = f"data/{filename}"
+    if file == 'listings':
+        csv_params = dict(
+            index_col='id',
+            parse_dates = ['host_since', 'first_review', 'last_review'],
+            low_memory = False,
+            nrows=nrows,
+            usecols=LISTINGS_COLUMNS,
+            )
+        filename = 'listings.csv'
     else:
-        path = f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}/{filename}"
-    df = pd.read_csv(path, nrows=nrows, )
+        return None
+    if local:
+        path = f"data/jan/{filename}"
+        df = pd.read_csv(path, **csv_params )
+    else:
+        fs = gcsfs.GCSFileSystem(project='PROJECT_NAME', token='/Users/ed/code/fivestar/star-project-key.json')
+        with fs.open(f'{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}/{filename}') as f:
+            df = pd.read_csv(f, **csv_params)
+    if file == 'listings':
+        df = df[(df['review_scores_rating'].notna()) & (df['number_of_reviews']>2)]
     return df
 
 
 if __name__ == "__main__":
-    params = dict(nrows=10,
-                  upload=False,
-                  local=False,  # set to False to get data from GCP (Storage or BigQuery)
-                  optimize=False)
+    params = dict(local=True)
+
     df = get_data(**params)
     print(df.shape)
