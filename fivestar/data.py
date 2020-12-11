7927f7527a903e02fdb634ef10e4b68f394a9599
Ed Landamore <mail@ratship.net>
2020-12-11 11:46:05 +0000

    Set data getter to pull from GCP if local datafiles not found

--- fivestar/data.py
+++ fivestar/data.py
@@ -8,7 +8,7 @@ from fivestar.params import BUCKET_NAME, BUCKET_TRAIN_DATA_PATH, PROJECT_NAME
 from fivestar.params import LISTINGS_COLUMNS
 from google.cloud import storage
 import gcsfs
-from os.path import dirname
+from os.path import dirname, isfile
 from pathlib import Path
 import fivestar
 
@@ -33,16 +33,21 @@ def get_data(file='listings', nrows=None, local=True, optimize=False, path=None,
     else:
         return None
 
-    if local:
-        if path:
-            path = path + filename
-        else:
-            path = f"{str(Path.home())}/code/OrthoLoess/fivestar/data/jan/{filename}"
+
+    if path:
+        path = path + filename
+    else:
+        path = f"{str(Path.home())}/code/OrthoLoess/fivestar/data/jan/{filename}"
+
+    if local and isfile(path):
         df = pd.read_csv(path, **csv_params )
     else:
-        fs = gcsfs.GCSFileSystem(project='PROJECT_NAME', token='/Users/ed/code/fivestar/star-project-key.json')
-        with fs.open(f'{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}/{filename}') as f:
-            df = pd.read_csv(f, **csv_params)
+        # fs = gcsfs.GCSFileSystem(project='PROJECT_NAME', token='/Users/ed/code/fivestar/star-project-key.json')
+        # with fs.open(f'{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}/{filename}') as f:
+        #     df = pd.read_csv(f, **csv_params)
+        # file_url = f'https://data.elandamore.net/fivestar/{BUCKET_TRAIN_DATA_PATH}/{filename}'
+        path = f'gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}/{filename}'
+        df = pd.read_csv(path, **csv_params )
     if file == 'listings':
         df = df[(df['review_scores_rating'].notna()) & (df['number_of_reviews']>2)]
     return df
